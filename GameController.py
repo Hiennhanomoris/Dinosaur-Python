f0a6f720042129629b5b30ef7513e383b94707c5
Phuc_Perseids <hiennhanomoris2001@gmail.com>
2021-10-19 12:59:47 +0700

    load background len man console

--- GameController.py
+++ GameController.py
@@ -1,5 +1,25 @@
 import pygame
 
+from class_background import Background
+
+# tao man console rong 600 cao 300
 pygame.init()
 screen = pygame.display.set_mode((600, 300))
 pygame.display.set_caption("dinosaur", icontitle=None)
+
+# tao bien can dung trong truong trinh
+endGame =  False
+clock = pygame.time.Clock()
+
+# khoi tao object mau
+background1 = Background(0, 0, "background.jpg")
+
+# ham load anh
+def LoadImage():
+    background_load = screen.blit(background1.pic, (background1.x_pos, background1.y_pos))
+
+# vong lap game
+while endGame == False:
+    clock.tick(120)                         # gioi han cho truong trinh chay 120f/s
+    screen.fill((255, 255, 255))            # do mau cho man hinh thanh trang
+    LoadImage()                             # load anh qua moi frame
--- class_background.py
+++ class_background.py
@@ -1,1 +1,11 @@
+import pygame
 
+class Background:
+
+    def __init__(self, x_pos, y_pos, pic):
+        self.x_pos = x_pos
+        self.y_pos = y_pos
+        self.pic = pygame.image.load(pic)
+
+    def Movement(self):
+        self.x_pos -= 5    
